057732239f97cdb249da27dfa963023e91eeec79
crycetruly <crycetruly@gmail.com>
2019-09-11 16:02:54 +0300

    ch(test): Add Unittest for All functionality

--- makethatapp/apps/lessons/views.py
+++ makethatapp/apps/lessons/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Lesson
 from .serializers import LessonSerializer
+
 
 class LessonViewSet(viewsets.ReadOnlyModelViewSet):
     """
